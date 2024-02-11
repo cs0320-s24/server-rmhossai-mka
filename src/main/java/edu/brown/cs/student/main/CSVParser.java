cdeb5311cdd371a38e94788e65b4aa3bb74d4715
Robayet Hossain <robayet_hossain@brown.edu>
2024-02-11 16:43:12 -0500

    doc: javadocs and commentations added for CSVParser class

--- src/main/java/edu/brown/cs/student/main/CSVParser.java
+++ src/main/java/edu/brown/cs/student/main/CSVParser.java
@@ -10,33 +10,58 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
+/**
+ * A CSV parser that reads from a Reader and creates objects using a CreatorFromRow strategy.
+ * The parser reads each line from the Reader and splits it into a list of strings using a regular expression.
+ * The parser then uses the CreatorFromRow strategy to create an object from each line.
+ * The parser can handle lines with different numbers of columns, but it will print a warning if the number of
+ * columns is not a equal for all likes.
+ * The parser throws a FactoryFailureException if the CreatorFromRow strategy fails to create an object.
+ *
+ * @param <T> - the type of objects created by the parser.
+ */
 public class CSVParser<T> {
 
+  // the reader from which the parser reads
   public BufferedReader reader;
-
+  // the strategy used by the parser to create objects from rows.
   public CreatorFromRow<T> strat;
-
-  /* REGEX CHECK:
-   I'm aware that this regex doesn't handle single quotes well.
-  */
-
+  // the regular expression used by the parser to create objects from rows.
   static final String regex = ",(?=([^\\\"]*\\\"[^\\\"]*\\\")*(?![^\\\"]*\\\"))";
 
+  /**
+   * Creates a new CSV parser that reads from the specified reader and uses the specified strategy to create objects.
+   *
+   * @param reader - the reader from which the parser reads.
+   * @param strat - the strategy used by the parser to create objects from rows.
+   */
   public CSVParser(Reader reader, CreatorFromRow<T> strat) {
     this.reader = new BufferedReader(reader);
     this.strat = strat;
   }
 
-  // String value, int id
+  /**
+   * Parses the input and creates a list of objects.
+   * The parser reads each line from the reader and splits it into a list of strings using the regex.
+   * The parser then uses the CreatorFromRow strategy to create an object from each line.
+   * If the number of columns is not equal for all lines, the parser will print a warning.
+   * If the CreatorFromRow strategy fails to create an object, the parser will throw a FactoryFailureException.
+   *
+   * @return - a list of objects created by the parser.
+   * @throws FactoryFailureException - if the CreatorFromRow strategy fails to create an object.
+   */
   public List<T> parse() throws FactoryFailureException {
-    // A FactoryFailureExpression is thrown to be handled by the caller, which may have a more
-    // informative error message.
-
+    // the size of the current row
     int rowSize;
+    // the expected size of each row
     int expectedRowSize = Integer.MAX_VALUE;
 
+    // the list of objects created by the parser
     List<T> mtrx = new ArrayList<>();
+    // the current line read from the reader
     String buf;
+    // read each line from the reader and split it into a list of strings
     try {
       while ((buf = reader.readLine()) != null) {
         List<String> splitLine = Arrays.asList(buf.split(regex));
