294951b34be2d615db7007a1e6b177b43044e444
Muhamed Ka <muhamedka@muhamedsair2514.devices.brown.edu>
2024-02-10 21:45:22 -0500

    feat: LoadCSVParser implemnented

--- src/main/java/edu/brown/cs/student/main/server/LoadCSVHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/LoadCSVHandler.java
@@ -1,10 +1,15 @@
 package edu.brown.cs.student.main.server;
 
 import edu.brown.cs.student.main.CSVParser;
+import edu.brown.cs.student.main.CreatorFromRow.StrListCreatorFromRow;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.List;
 import java.util.Map;
 
@@ -12,12 +17,22 @@ public class LoadCSVHandler implements Route {
 
     CSVParser<List<String>> parser;
 
+    public LoadCSVHandler(CSVParser parser){
+        this.parser = parser;
+    }
+
     @Override
     public Object handle(Request request, Response response) throws Exception {
 
-        String csvFilePath = request.queryParams("soupName");
-
-        return null;
+        String csvFilePath = request.queryParams("loadCSV");
+        FileReader reader = null;
+        try {
+             reader = new FileReader(csvFilePath);
+        } catch (FileNotFoundException e) {
+            System.err.println("error");
+        }
+        parser = new CSVParser<>(new BufferedReader(reader),
+                new StrListCreatorFromRow());
+        return parser;
     }
-
 }
