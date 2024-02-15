cfb08c682b1e6db318bd78adaf0ff2aacc68afdc
Robayet Hossain <robayet_hossain@brown.edu>
2024-02-15 17:35:22 -0500

    docs: CSVData docs updated

--- src/main/java/edu/brown/cs/student/main/DataSource/CSVData.java
+++ src/main/java/edu/brown/cs/student/main/DataSource/CSVData.java
@@ -1,6 +1,17 @@
 package edu.brown.cs.student.main.DataSource;
 
+/**
+ * Quick Summary:
+ * Represents CSV data in the form of a matrix.
+ * Stores a matrix of strings representing CSV data.
+ */
+
 import java.util.List;
+
+/**
+ * Represents CSV data in the form of a matrix.
+ * @param mtrx - the matrix to set as the current CSV matrix.
+ */
 public record CSVData(List<List<String>> mtrx) {
 
 }
