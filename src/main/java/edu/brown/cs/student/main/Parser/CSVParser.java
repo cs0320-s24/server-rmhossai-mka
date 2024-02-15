9a49468285f9bc755c85a903ed0408d3692997f5
Robayet Hossain <robayet_hossain@brown.edu>
2024-02-15 17:26:31 -0500

    docs: csvparser docs updated

--- src/main/java/edu/brown/cs/student/main/Parser/CSVParser.java
+++ src/main/java/edu/brown/cs/student/main/Parser/CSVParser.java
@@ -10,14 +10,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 /**
+ * Quick Summary:
  * A CSV parser that reads from a Reader and creates objects using a CreatorFromRow strategy.
  * The parser reads each line from the Reader and splits it into a list of strings using a regular expression.
  * The parser then uses the CreatorFromRow strategy to create an object from each line.
  * The parser can handle lines with different numbers of columns, but it will print a warning if the number of
  * columns is not a equal for all likes.
  * The parser throws a FactoryFailureException if the CreatorFromRow strategy fails to create an object.
+ */
+
+/**
+ * A CSV parser that reads from a Reader and creates objects using a CreatorFromRow strategy.
  *
  * @param <T> - the type of objects created by the parser.
  */
@@ -41,6 +45,9 @@ public class CSVParser<T> {
     this.strat = strat;
   }
 
+  /**
+   * Constructs a CSVParser with null reader and strategy.
+   */
   public CSVParser() {
     this.reader = null;
     this.strat = null;
@@ -58,8 +65,7 @@ public class CSVParser<T> {
    */
   public List<T> parse() throws FactoryFailureException {
     if (this.reader == null || this.strat == null) {
-      System.err.println("Either reader or parse strategy uninitialized. " +
-              "Please use /load to load in a CSV file");
+      System.err.println("Either reader or parse strategy uninitialized. " + "Please use /load to load in a CSV file");
       return new ArrayList<>();
     }
     // the size of the current row
@@ -80,8 +86,7 @@ public class CSVParser<T> {
         if (expectedRowSize == Integer.MAX_VALUE) expectedRowSize = rowSize;
         if (rowSize != expectedRowSize) {
           System.err.println(
-              "Warning: Columns are not equal! If you intend "
-                  + "to search, the search may break!");
+              "Warning: Columns are not equal! If you intend " + "to search, the search may break!");
         }
       }
       reader.close();
